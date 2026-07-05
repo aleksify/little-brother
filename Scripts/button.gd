ca253280223eedbac11fb45ea2f3b3337dbfb71f
mogi <git@mogiya.dev>
2026-07-05 13:30:25 +0100

    add test preload scene

---
+++ Scripts/button.gd
@@ -0,0 +1,18 @@
+extends Button
+
+
+@onready var channel_scene: Node2D = get_parent()
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_pressed() -> void:
+	channel_scene.go_to_second()
